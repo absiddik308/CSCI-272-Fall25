a4f37f0b789cb08d7da4b2c36568ace66dd54305
absiddik308 <absiddik308@gmail.com>
2025-09-01 20:51:36 -0400

    Add files via upload

---
+++ Week-1/welcome user 2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>   
+using namespace std;  
+int main() {
+    string name; 
+
+    cout << "What is your name? ";  
+    cin >> name;   
+
+    cout << "Hi, " << name << "! Welcome to John Jay." << endl;  
+
+    return 0;  
+}
